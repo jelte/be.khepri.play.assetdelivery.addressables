d5d05f47d278cc90bd2934a19be9e28f88789229
jelte.steijaert <Jelte.Steijaert@sega.co.uk>
2022-10-04 12:29:06 +0100

    All asset bundles also in base_assets when using Unity 2021 #62

--- Editor/PlayAssetPackBundlesPreprocessor.cs
+++ Editor/PlayAssetPackBundlesPreprocessor.cs
@@ -2,14 +2,53 @@
 using System.Linq;
 using Khepri.PlayAssetDelivery.Editor;
 using UnityEditor;
+using UnityEditor.AddressableAssets.Settings;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine.AddressableAssets;
 using UDebug = UnityEngine.Debug;
 
+#if UNITY_2021_2_OR_NEWER
+public class PlayAssetPackBundlesPreprocessor : BuildPlayerProcessor
+{
+    public override int callbackOrder => -1;
+    
+    public override void PrepareForBuild(BuildPlayerContext buildPlayerContext)
+    {
+        AddressablesPlayerBuildProcessor.BuildAddressablesOverride = settings =>
+        {
+            AddressableAssetSettings.BuildPlayerContent(out var result);
+            PostBuild(buildPlayerContext);
+            return result;
+        };
+    }
+
+    private void PostBuild(BuildPlayerContext buildPlayerContext)
+    {
+        var outputPathExtension = Path.GetExtension(buildPlayerContext.BuildPlayerOptions.locationPathName);
+        if (buildPlayerContext.BuildPlayerOptions.target != BuildTarget.Android || !outputPathExtension.Equals(".aab"))
+        {
+            AssetPackBuilder.ClearConfig();
+            return;
+        }
+        var bundles = AssetPackBuilder.GetBundles(Addressables.BuildPath, SearchOption.AllDirectories);
+        if (bundles.Length == 0)
+        {
+            UDebug.Log($"[{nameof(PlayAssetPackBundlesPreprocessor)}.{nameof(PrepareForBuild)}] No bundles removed.");
+            return;
+        }
+        foreach (var bundle in bundles)
+        {
+            bundle.DeleteFile();
+        }
+
+        UDebug.Log($"[{nameof(PlayAssetPackBundlesPreprocessor)}.{nameof(PrepareForBuild)}] Removed: \n -{string.Join("\n -", bundles.Select(bundle => bundle.Bundle))}");
+    }
+}
+#else
 public class PlayAssetPackBundlesPreprocessor : IPreprocessBuildWithReport
 {
-    public int callbackOrder => 2;
+    public int callbackOrder => 0;
 
     public void OnPreprocessBuild(BuildReport report)
     {
@@ -19,7 +58,7 @@ public class PlayAssetPackBundlesPreprocessor : IPreprocessBuildWithReport
             AssetPackBuilder.ClearConfig();
             return;
         }
-        var bundles = AssetPackBuilder.GetBundles(Addressables.PlayerBuildDataPath, SearchOption.AllDirectories);
+        var bundles = AssetPackBuilder.GetBundles(Addressables.BuildPath, SearchOption.AllDirectories);
         if (bundles.Length == 0)
         {
             UDebug.Log($"[{nameof(PlayAssetPackBundlesPreprocessor)}.{nameof(OnPreprocessBuild)}] No bundles removed.");
@@ -32,3 +71,4 @@ public class PlayAssetPackBundlesPreprocessor : IPreprocessBuildWithReport
         UDebug.Log($"[{nameof(PlayAssetPackBundlesPreprocessor)}.{nameof(OnPreprocessBuild)}] Removed: \n -{string.Join("\n -", bundles.Select(bundle => bundle.Bundle))}");
     }
 }
+#endif
